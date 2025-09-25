fce065eae3c23caf2cfc2918cbd47fd20d461cb3
anastasiia_obolonska <velentiy@gmail.com>
2025-09-25 16:36:26 +0300

    fix

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,5 @@
 import createHttpError from 'http-errors';
 
 export default function notFoundHandler(req, res, next) {
-  next(new createHttpError.NotFound('Contact not found'));
+  next(new createHttpError.NotFound('Route not found'));
 }
